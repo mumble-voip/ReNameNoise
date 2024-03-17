8e6bf843630a54ba149f087a7041bfe28c5e5d64
Hartmnt <hartmunt@protonmail.com>
2024-03-17 13:22:09 +0000

    REFAC: Rename symbol opus_alloc -> renamenoise_alloc2

--- src/renamenoise_fft.h
+++ src/renamenoise_fft.h
@@ -34,7 +34,7 @@
 #include "arch.h"
 
 #include <stdlib.h>
-#define opus_alloc(x) malloc(x)
+#define renamenoise_alloc2(x) malloc(x)
 #define opus_free(x) free(x)
 
 #ifdef __cplusplus
@@ -46,7 +46,7 @@ extern "C" {
 # define kiss_fft_scalar __m128
 #define KISS_FFT_MALLOC(nbytes) memalign(16,nbytes)
 #else
-#define KISS_FFT_MALLOC opus_alloc
+#define KISS_FFT_MALLOC renamenoise_alloc2
 #endif
 
 #ifndef kiss_fft_scalar
