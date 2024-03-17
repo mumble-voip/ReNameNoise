65b3ea841f499b21e1509cead5292255bbe9fb25
Hartmnt <hartmunt@protonmail.com>
2024-03-17 11:16:32 +0000

    REFAC: Rename symbol NEG16 -> RENAMENOISE_NEG16

--- src/arch.h
+++ src/arch.h
@@ -129,7 +129,7 @@ static RENAMENOISE_INLINE int renamenoise_isnan(float x)
 #define RENAMENOISE_QCONST16(x,bits) (x)
 #define RENAMENOISE_QCONST32(x,bits) (x)
 
-#define NEG16(x) (-(x))
+#define RENAMENOISE_NEG16(x) (-(x))
 #define NEG32(x) (-(x))
 #define NEG32_ovflw(x) (-(x))
 #define EXTRACT16(x) (x)
