4dce9428a550f2e58f0e7889a068a0142c625817
Hartmnt <hartmunt@protonmail.com>
2024-03-18 10:20:57 +0000

    REFAC: Rename symbol PITCH_FRAME_SIZE -> RENAMENOISE_PITCH_FRAME_SIZE

--- src/denoise.c
+++ src/denoise.c
@@ -48,8 +48,8 @@
 
 #define RENAMENOISE_PITCH_MIN_PERIOD 60
 #define RENAMENOISE_PITCH_MAX_PERIOD 768
-#define PITCH_FRAME_SIZE 960
-#define PITCH_BUF_SIZE (RENAMENOISE_PITCH_MAX_PERIOD+PITCH_FRAME_SIZE)
+#define RENAMENOISE_PITCH_FRAME_SIZE 960
+#define PITCH_BUF_SIZE (RENAMENOISE_PITCH_MAX_PERIOD+RENAMENOISE_PITCH_FRAME_SIZE)
 
 #define SQUARE(x) ((x)*(x))
 
@@ -326,12 +326,12 @@ static int compute_frame_features(ReNameNoiseDenoiseState *st, renamenoise_fft_c
   RENAMENOISE_COPY(&st->pitch_buf[PITCH_BUF_SIZE-RENAMENOISE_FRAME_SIZE], in, RENAMENOISE_FRAME_SIZE);
   pre[0] = &st->pitch_buf[0];
   renamenoise_pitch_downsample(pre, pitch_buf, PITCH_BUF_SIZE, 1);
-  renamenoise_pitch_search(pitch_buf+(RENAMENOISE_PITCH_MAX_PERIOD>>1), pitch_buf, PITCH_FRAME_SIZE,
+  renamenoise_pitch_search(pitch_buf+(RENAMENOISE_PITCH_MAX_PERIOD>>1), pitch_buf, RENAMENOISE_PITCH_FRAME_SIZE,
                RENAMENOISE_PITCH_MAX_PERIOD-3*RENAMENOISE_PITCH_MIN_PERIOD, &pitch_index);
   pitch_index = RENAMENOISE_PITCH_MAX_PERIOD-pitch_index;
 
   gain = renamenoise_remove_doubling(pitch_buf, RENAMENOISE_PITCH_MAX_PERIOD, RENAMENOISE_PITCH_MIN_PERIOD,
-          PITCH_FRAME_SIZE, &pitch_index, st->last_period, st->last_gain);
+          RENAMENOISE_PITCH_FRAME_SIZE, &pitch_index, st->last_period, st->last_gain);
   st->last_period = pitch_index;
   st->last_gain = gain;
   for (i=0;i<RENAMENOISE_WINDOW_SIZE;i++)
